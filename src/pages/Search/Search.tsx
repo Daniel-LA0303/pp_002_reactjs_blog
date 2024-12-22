172f763183d3bc488e9759788992705c030e4b4c
Luis Alberto <03daniel030399@gmail.com>
2024-12-21 21:34:13 -0600

    add a basic ui in serach page

---
+++ src/pages/Search/Search.tsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+function TabMenu() {
+    const [activeTab, setActiveTab] = useState("blog");
+
+    const renderContent = () => {
+        switch (activeTab) {
+            case "blog":
+                return <div>Bienvenido al Home</div>;
+            case "category":
+                return <div>Configuraciones de usuario</div>;
+            case "user":
+                return <div>Perfil del usuario</div>;
+            default:
+                return <div>Selecciona una pestaña</div>;
+        }
+    };
+
+    return (
+        <div className="flex gap-4 w-10/12  font-[sans-serif] mx-auto">
+            <ul className="hidden md:block md:w-3/12  bg-gray-100  py-5 rounded-md">
+                <li
+                    id="homeTab"
+                    className={`tab flex items-center text-sm ${
+                        activeTab === "blog" ? "font-semibold bg-white text-blue-600" : "text-gray-800"
+                    } hover:text-blue-600 py-5 px-5 cursor-pointer transition-all`}
+                    onClick={() => setActiveTab("blog")}
+                >
+                    Blogs
+                </li>
+                <li
+                    id="settingTab"
+                    className={`tab flex items-center text-sm ${
+                        activeTab === "category" ? "font-semibold bg-white text-blue-600" : "text-gray-800"
+                    } hover:text-blue-600 py-5 px-5 cursor-pointer transition-all`}
+                    onClick={() => setActiveTab("category")}
+                >
+                    Categories
+                </li>
+                <li
+                    id="profileTab"
+                    className={`tab flex items-center text-sm ${
+                        activeTab === "user" ? "font-semibold bg-white text-blue-600" : "text-gray-800"
+                    } hover:text-blue-600 py-5 px-5 cursor-pointer transition-all`}
+                    onClick={() => setActiveTab("user")}
+                >
+                    Users
+                </li>
+            </ul>
+
+            {/* Contenedor del contenido */}
+            <div className="md:w-8/12 p-3 flex-grow bg-white shadow-lg rounded-lg">{renderContent()}</div>
+        </div>
+    );
+}
+
+export default TabMenu;
--- src/routers/Routes.tsx
+++ src/routers/Routes.tsx
@@ -2,12 +2,21 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import About from '../pages/About';
+import Search from '../pages/Search/Search';
 
 
 const AppRoutes: React.FC = () => {
   return (
     <Routes>
       <Route path="/about" element={<About />} />
+
+
+
+
+
+
+
+      <Route path='/search' element={<Search />} />
       
     </Routes>
   );
